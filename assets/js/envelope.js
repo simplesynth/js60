6c2e29d0bd2f2c19a8f04ff8273826784c05ff93
Trevor Mast <trevormast@gmail.com>
2018-01-25 22:45:56 -0800

    working on gain envelope

--- assets/js/envelope.js
+++ assets/js/envelope.js
@@ -7,37 +7,43 @@ class Envelope {
     this._amplitude = 0.5;
     // times in milliseconds
     this._attackTime = 200;
-    this._delayTime = 1;
+    this._delayTime = 100;
     // 0-1
     this._sustainLevel = 0.5;
     this._continue = false;
   }
 
   start() {
-    this._continue = true;
     this._destination.value = 0;
+    this._continue = true;
     this.attackCycle(0);
-    this.delayCycle(0)
   }
 
   attackCycle(count) {
     var self = this;
     setTimeout(function() {
-      // console.log(self._destination.value)
-      self._destination.value += parseInt(self._destinationBaseValue) / parseInt(self._attackTime);
-      // console.log(self._destination.value)
-      if((count < self._attackTime) && self._continue === true) { self.attackCycle(count + 1)}
+      if (this._continue === false) {
+        self._destination.value = 0;
+      }
+      else{
+        // console.log(self._destination.value)
+        self._destination.value += parseInt(self._destinationBaseValue) / parseInt(self._attackTime);
+        // console.log(self._destination.value)
+        if(count < self._attackTime) { self.attackCycle(count + 1)}
+        else  { self.delayCycle(0) }
+      }
     }, 1);
   }
 
   delayCycle(count) {
     var self = this;
     setTimeout(function() {
+      if (this._continue === false) { self._destination.value = 0; clearTimeout(this);}
       // console.log(self._destination.value)
       var distanceToSustainLevel = parseInt(self._destinationBaseValue) - (parseInt(self._destinationBaseValue) * parseInt(self._sustainLevel))
       self._destination.value -= distanceToSustainLevel / parseInt(self._delayTime)
       // console.log(self._destination.value)
-      if((count < self._attackTime) && self._continue === true) { self.attackCycle(count + 1)}
+      if(count < self._delayTime) { self.delayCycle(count + 1)}
     }, 1);
   }
 
